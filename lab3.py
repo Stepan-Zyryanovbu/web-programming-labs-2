8606e504950da494ee973eac4f26265192f37a18
Иван Иванов <spznsk@inbox.ru>
2024-10-25 23:26:47 +0700

    Настройки отоюражения

--- lab3.py
+++ lab3.py
@@ -69,3 +69,33 @@ def pay():
 def success():
     price = request.args.get('price')  # Получение стоимости из параметра
     return render_template('lab3/success.html', price=price)
+
+
+@lab3.route('/lab3/settings')
+def settings():
+    color = request.args.get('color')
+    bg_color = request.args.get('bg_color')
+    font_size = request.args.get('font_size')
+    
+    # Создаем ответ с редиректом, чтобы установить cookies
+    resp = make_response(redirect('/lab3/settings'))
+    
+    # Устанавливаем cookies для цвета текста, цвета фона и размера шрифта, если они заданы
+    if color:
+        resp.set_cookie('color', color)
+    if bg_color:
+        resp.set_cookie('bg_color', bg_color)
+    if font_size:
+        resp.set_cookie('font_size', font_size)
+    
+    # Если нет новых параметров, загружаем страницу с текущими значениями cookies
+    if not (color or bg_color or font_size):
+        color = request.cookies.get('color')
+        bg_color = request.cookies.get('bg_color')
+        font_size = request.cookies.get('font_size')
+        resp = make_response(render_template('lab3/settings.html', color=color, bg_color=bg_color, font_size=font_size))
+    
+    return resp
+
+
+
