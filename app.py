916deeafd44c04aa65bd4724e578fdf2025eb8f9
Иван Иванов <spznsk@inbox.ru>
2024-10-25 19:36:01 +0700

    Заготовка к третьей лабораторной работе

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, redirect, url_for, render_template
 from lab1 import lab1
 from lab2 import lab2
+from lab3 import lab3
 
 app = Flask(__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
+app.register_blueprint(lab3)
 
 @app.route("/")
 @app.route("/index")
@@ -13,30 +15,7 @@ def start():
 
 @app.route("/menu")
 def menu():
-    return """
-<!doctype html>
-<html>
-    <head>
-        <title>НГТУ, ФБ, Лабораторные работы</title>
-    </head>
-    <body>
-        <header>
-            НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
-        </header>
-
-        <nav>
-            <ul>
-                <li><a href="/lab1">Первая лабораторная</a></li>
-                <li><a href="/lab2">Вторая лабораторная</a></li>
-            </ul>
-        </nav>
-
-        <footer>
-            &copy; Зырянов Степан Павлович, ФБИ-24, 3 курс, 2024
-        </footer>
-    </body>
-</html>
-    """
+    return ('menu.html')
 
 
 
