f3f5828dbb4bd23621f90d052ebd7958a3b44c4b
Иван Иванов <spznsk@inbox.ru>
2024-12-06 22:02:16 +0700

    Заготовка к 6 лабе

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from lab2 import lab2
 from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
+from lab6 import lab6
 import os
 
 app = Flask(__name__)
@@ -16,6 +17,7 @@ app.register_blueprint(lab2)
 app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
+app.register_blueprint(lab6)
 
 @app.route('/')
 @app.route('/index')
@@ -49,7 +51,13 @@ def menu():
         <div>
             <a href='http://127.0.0.1:5000/lab4'>Лабораторная работа 4</a>
         </div>
-
+        <div>
+            <a href='http://127.0.0.1:5000/lab5'>Лабораторная работа 5</a>
+        </div>
+        <div>
+            <a href='http://127.0.0.1:5000/lab6'>Лабораторная работа 6</a>
+        </div>
+        
         <footer>
             &copy; Зырянов Степан, ФБИ-24, 3 курс, 2024
         </footer>
