a7df873a2ce625ae26f0d348cb5fa29700ecadc5
Зырянов Степан <spznsk@inbox.ru>
2024-12-27 18:20:52 +0700

    модель базы данных

--- lab8.py
+++ lab8.py
@@ -1,8 +1,6 @@
 from flask import Blueprint, render_template, redirect, request
 from db import db
-from db.models import users,articles
 from werkzeug.security import check_password_hash, generate_password_hash
-from flask_login import login_user, login_required, current_user, logout_user
 
 lab8 = Blueprint('lab8', __name__)
 @lab8.route('/lab8/')
